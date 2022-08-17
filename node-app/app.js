176723734f4f9c0bca2ef230b297f8351d2cbfb6
sachinkumawatcoindcx <sachin.kumawat@coindcx.com>
2022-08-17 21:53:27 +0530

    mid

--- node-app/app.js
+++ node-app/app.js
@@ -88,7 +88,70 @@ app.get("/coinlist/:id", urlencodedParser, (req, res)=>{
         console.log(result);
         res.send(result);
     })
-   
+})
+
+app.get("/user/developer/coinlist", urlencodedParser, (req, res)=>{
+    console.log("api to /user/developer/coinlist");
+    let devid = 1;
+    let sqlInsert = 'select * from coinlist where dev_id=' + devid;
+    let query = con.query(sqlInsert, (err, result) => {
+        if(err) {
+            throw err;
+        }
+        console.log(result);
+        // res.send(result);
+    })
+})
+
+app.post("/user/developer/listnewcoin", urlencodedParser, (req, res)=>{
+    console.log("api to /user/developer/listnewcoin");
+    let sd = new Date();
+    let ed = new Date();
+    ed.setDate(sd.getDate() + 10);
+    sd = sd.toISOString().split('T')[0]
+    ed = ed.toISOString().split('T')[0]
+    console.log(sd, " asfdad "+ ed);
+    let newcoindata = {
+        dev_id: 1,
+        coin_name: "sample coin_name",
+        about: "sample about",
+        start_date: sd,
+        end_date: ed,
+        start_price: 15,
+        end_price: 25,
+        platform:"BTC",
+        category:"Blockchain",
+        total_tokens: 15,
+        min_tokens: 15,
+        white_paper: "sample whitepaper"
+    };
+    console.log(newcoindata);
+    let sqlInsert = `insert into coinlist(dev_id, coin_name, about, start_date, end_date, start_price, end_price, platform, category, total_tokens, min_tokens, white_paper) values('${newcoindata.dev_id}', '${newcoindata.coin_name}', '${newcoindata.about}', '${newcoindata.start_date}', '${newcoindata.end_date}', ${newcoindata.start_price}, ${newcoindata.end_price}, '${newcoindata.platform}','${newcoindata.category}', ${newcoindata.total_tokens}, ${newcoindata.min_tokens},'${newcoindata.white_paper}')`;
+    console.log(sqlInsert);
+    let query = con.query(sqlInsert, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send({"statusCode":200, "errMsg": "no error"});
+    })
+})
+
+app.get("/bid/:investorid", urlencodedParser, (req, res)=>{
+    console.log("api to /bid/:investorid");
+    let investorid = 1;
+    let sqlInsert = `select * from bid where user_id=${investorid}`;
+    let query = con.query(sqlInsert, (err, result) => {
+        if(err) {
+            throw err;
+        }
+        console.log(result);
+        if(result.length == 0){
+            res.send({"statusCode":200, "ifExists": false, "errMsg": "no error"});
+        }
+        else{
+            res.send({"statusCode":200, "ifExists": true, "errMsg": "Error User already placed bid"});
+        }
+        // res.send(result);
+    })
 })
 
 app.listen(3001, () => {
