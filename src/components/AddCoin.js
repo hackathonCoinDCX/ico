a764bf6d6479c6e3d750a52ef0c4ada2180d4ba1
Amannnnn30 <aman1.kumar@coindcx.com>
2022-08-17 23:08:51 +0530

    addcoin + available coin + dev dahboard

---
+++ src/components/AddCoin.js
@@ -0,0 +1,91 @@
+import React from "react";
+
+class AddCoin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    };
+  }
+  handleChange = (event) => {
+    // alert(event.target.value)
+    this.setState({ [event.target.name]: event.target.value });
+  };
+  handleQuantityChange(event) {}
+
+  componentDidMount() {
+  }
+
+  handleSubmit = (event) => {
+    console.log("request success");
+    console.log(event);
+    event.preventDefault();
+  };
+
+  
+  render() {
+    // const { DataisLoaded, selectedCoin } = this.state;
+    // if (!DataisLoaded)
+    //   return (
+    //     <div>
+    //       <h1> Pleses wait some time.... </h1>{" "}
+    //     </div>
+    //   );
+
+    return (
+      <div>
+        <h1>List a new coin</h1>
+        <form>
+            <table>
+                <tr> 
+                    <td>Enter Name</td>
+                    <td><input type='string'></input></td>
+                </tr>
+                <tr> 
+                    <td>about</td>
+                    <td><input type='string'></input></td>
+                </tr>
+                <tr> 
+                    <td>start date</td>
+                    <td><input type='string'></input></td>
+                </tr>
+                <tr> 
+                    <td>end date</td>
+                    <td><input type='string'></input></td>
+                </tr>
+                <tr> 
+                    <td>start price</td>
+                    <td><input type='string'></input></td>
+                </tr>
+                <tr> 
+                    <td>end price</td>
+                    <td><input type='string'></input></td>
+                </tr>
+                <tr> 
+                    <td>Platform</td>
+                    <td><input type='string'></input></td>
+                </tr>
+                <tr> 
+                    <td>Category</td>
+                    <td><input type='string'></input></td>
+                </tr>
+                <tr> 
+                    <td>total token</td>
+                    <td><input type='string'></input></td>
+                </tr>
+                <tr> 
+                    <td>min token</td>
+                    <td><input type='string'></input></td>
+                </tr>
+                <tr> 
+                    <td>upload white_paper</td>
+                    <td><input type='string'></input></td>
+                </tr>
+            </table>
+        </form>
+        
+      </div>
+    );
+  }
+}
+export default AddCoin;
